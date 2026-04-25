d1e020d17c53d816ba3316f230c88df5d0e0ae4f
Engr-Zihad <mojahidulislamzihad686@gmail.com>
2026-04-25 12:55:05 +0300

    add precedence

--- math.c
+++ math.c
@@ -10,12 +10,18 @@ int main(){
     double div=a/b;
     double mod=fmod(a,b);
     double power=pow(a,b);
-    printf("the addition is:%lf\n",sum);
-    printf("the subtraction is:%lf\n",sub);
-    printf("the multiplication is:%lf\n",mul);
-    printf("the division is:%lf\n",div);
-    printf("the modulus is:%lf\n",mod);
-    printf("the power is:%lf\n",power);
+    printf("the addition is:%0.2lf\n",sum);
+    printf("the subtraction is:%0.2lf\n",sub);
+    printf("the multiplication is:%0.2lf\n",mul);
+    printf("the division is:%0.2lf\n",div);
+    printf("the modulus is:%0.2lf\n",mod);
+    printf("the power is:%0.2lf\n",power);
+    //operator precedence BODMAS and left to right associativity
+    double c ,d ;
+    printf("enter two number :");
+    scanf("%lf%lf",&c,&d);
+     double result=a+b/c*d;
+     printf("the result of (a+b/c*d) operator precedence is:%0.2lf\n",result);
     return 0;
 
 }
